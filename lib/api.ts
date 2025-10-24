47e5eb6eaff45844addf5170be0abad35303d1f3
ashiwanikumar <ashvanikumar109@gmail.com>
2025-10-24 09:22:12 +0530

    mod: company account page

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8004/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8004/api/v1';
 
 // Create axios instance
 const api = axios.create({
